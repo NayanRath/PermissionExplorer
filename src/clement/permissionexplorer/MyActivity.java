648686fbb87bc18ab8eaac04b4c4376872d23592
Clément Dagneau <clement.dagneau@gmail.com>
2012-06-18 18:14:58 +0100

    First attempt at loading permissions description from JSON file.

--- src/clement/permissionexplorer/MyActivity.java
+++ src/clement/permissionexplorer/MyActivity.java
@@ -10,7 +10,14 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MyActivity extends ListActivity
 {
@@ -27,6 +34,43 @@ public class MyActivity extends ListActivity
 
         PackageInfoAdapter adapter = new PackageInfoAdapter(this, appinstall);
         setListAdapter(adapter);
+
+        InputStream is = getResources().openRawResource(R.raw.permissions);
+
+        InputStreamReader inputreader = new InputStreamReader(is);
+        BufferedReader buffreader = new BufferedReader(inputreader);
+        String line;
+        StringBuilder text = new StringBuilder();
+
+        try
+        {
+            while (( line = buffreader.readLine()) != null)
+            {
+                text.append(line);
+                text.append('\n');
+            }
+        }
+        catch (IOException e)
+        {
+        }
+
+        /*
+        try
+        {
+
+            JSONObject jsonArray = new JSONObject(new String(text));
+            //jsonArray = jsonArray.getJSONObject("permission");
+            //JSONArray array = jsonArray.getJSONArray("permission");
+            jsonArray = jsonArray;
+            for (int i = 0; i < jsonArray.length(); i++)
+            {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+            }
+        } catch (JSONException e)
+        {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        */
     }
 
     @Override
