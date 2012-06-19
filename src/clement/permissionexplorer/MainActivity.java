bb9cc88b3840ae60eeb2547010d3eacb1d091c18
Clément Dagneau <clement.dagneau@gmail.com>
2012-06-19 13:36:07 +0100

    Renamed activity.

--- src/clement/permissionexplorer/MainActivity.java
+++ src/clement/permissionexplorer/MainActivity.java
@@ -1,26 +1,15 @@
 package clement.permissionexplorer;
 
-import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.List;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-public class MyActivity extends ListActivity
+public class MainActivity extends ListActivity
 {
     private List<PackageInfo> appinstall;
 
